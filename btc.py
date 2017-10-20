a14da2462578717da53437ff9a53e94467ed11a9
Conner Bean <beanconn@msu.edu>
2017-10-20 10:48:18 -0400

    bitcoin file that utilizes coindesk API for pricing

---
+++ btc.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+_CoinDeskAPI = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+
+def ChangeTZ(string):
+    string = string[:string.index('UTC')] + "EST"
+
+    hour = string[string.index(':') - 2:string.index(':')]
+    hour = str((int(hour) - 4) % 24).zfill(2)
+    return (string[:string.index(':') - 2] + hour + string[string.index(':'):])
+
+
+def main():
+    r = requests.get(_CoinDeskAPI)
+    btcJson = r.json()
+
+    updatedTime = ChangeTZ(btcJson['time']['updated'])
+
+    btcPrice = "$" + btcJson['bpi']['USD']['rate'][:-2]
+
+    infoString = "As of " + updatedTime + ", the price of Bitcoin is: " + btcPrice
+    print(infoString)
---
+++ main.py
@@ -0,0 +1,9 @@
+# Main file for RemindMe Application
+# Users will run this, and input their data 
+#  and store it in DB
+# 
+# After, we will use this to schedule reminders
+#
+# \Author: Conner S. Bean
+# circa October 2017
+
