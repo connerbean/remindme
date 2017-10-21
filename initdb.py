9b5dd1be4d3583c014c313a5560b28720b2ac2be
Conner Bean <beanconn@msu.edu>
2017-10-21 18:17:05 -0400

    Changed date format to text

--- initdb.py
+++ initdb.py
@@ -4,7 +4,7 @@ import sqlite3
 connection = sqlite3.connect('database.db')
 print('Opened database successfully')
 
-connection.execute('CREATE TABLE reminders (day DATE, remind TEXT)')
+connection.execute('CREATE TABLE reminders (day TEXT, remind TEXT)')
 print('Table created successfully')
 
 connection.close()
